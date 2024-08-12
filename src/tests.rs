bfd00d75c5c97427ef5950259d672196f7d334cb
Bridget <isaacpasco@icloud.com>
2024-08-12 15:13:03 -0700

    lmao

---
+++ src/tests.rs
@@ -0,0 +1,13 @@
+use crate::*;
+const PRG1: &str = "{hello world}";
+const PRG2: &str = "{{hello world}}";
+#[test]
+fn test_end_scope() {
+    let tokens = tokenize(PRG1);
+    eprintln!("tokens:{:#?}", tokens);
+    let end = calc_close_scope(&tokens, 0).expect("prg1 should work");
+    assert!(end == 3);
+    let tokens = tokenize(PRG2);
+    let end = calc_close_scope(&tokens, 0).expect("prg2 should work");
+    assert!(end == 5);
+}
