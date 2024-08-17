3e6b33dcd216f93bc4372581a52e402d87f3c5a1
Bridget Hartin Pasco <isaacpasco@icloud.com>
2024-08-16 18:56:45 -0700

    bruh

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,11 +1,16 @@
 use crate::parser::*;
 use std::fs;
 use std::env;
-pub fn compile_type(name:String, data:Type)->Result<String, ()>{
-    return Err(());
+pub fn compile_type_header(name:String, data:Type)->Result<String, ()>{
+    Err(())
 }
+pub fn compile_type(name:String, data:Type)->Result<String, ()>{
+    let mut out =String::from("");
+    Err(())
+} 
 pub fn compile(prog:Program)->Result<(),()>{
     let mut out = String::from("");
+    let mut typedecs = "";
     for i in &prog.types{
         out += &compile_type(i.0.clone(), i.1.clone())?;
     };
