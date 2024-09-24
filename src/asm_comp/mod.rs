d87f792824052efb35ad1ad192be9a969ec11074
Bridget Hartin Pasco <isaacpasco@icloud.com>
2024-09-23 19:28:52 -0700

    IT WORKS IN BUG

--- src/asm_comp/mod.rs
+++ src/asm_comp/mod.rs
@@ -129,7 +129,7 @@ pub fn compile_function(
                 }
                 out += &format!(
                     "   mov r10, QWORD[rbp+{}]\n",
-                    (stack_arg_size - stack_arg_count) as i64+8
+                    stack_arg_count as i64+16
                 );
                 out += &format!("   mov QWORD[rbp-{}], r10\n", base_ptr+arg_sz-v);
                 stack_arg_count += 8;
