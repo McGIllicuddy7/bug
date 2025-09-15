8dccbdf62b067a6fd5267a8c4e25afb24ccdb087
Bridget Hartin Pasco <isaacpasco@icloud.com>
2025-09-15 14:40:19 -0700

    BRUH

--- bug.h
+++ bug.h
@@ -35,16 +35,50 @@ typedef struct {
 	const char * base;
 	Arena * arena;
 }TokenStream;
+typedef enum{
+	o_add, 
+	o_sub, 
+	o_div, 
+	o_mul, 
+	o_asn, 
+}OpType;
 typedef enum {
-	NodeIndent, 
-	NodeFuncCall,
-	NodeEq,
-	NodeBinop,
-	NodeList,
-}NodeType;
+	v_immf,
+	v_immi,
+	v_stc,
+	v_nm,
+	v_take_ref, 
+	v_deref,
+	v_index,
+}VarType;
+typedef struct Operand{
+	VarType type;
+	int i;
+	float f;
+	int idx;
+	Str nm;
+	struct Operand * child;
+}Operand;
 
+typedef struct {
+	OpType type;
+	Operand * arguments;
+	size_t arg_count;
+	long output;
+}Operation;
+enable_vec_type(Operation);
+typedef struct{
+	OperationVec operations;
+}Expr;
+enable_result(Expr);
+typedef enum {
+	If, 
+	While,
+}StatementType;
 TokenResult next_token(TokenStream * strm);
 TokenResult peek_token(TokenStream * strm);
 void print_token(Token t);
 TokenStream create_token_stream(Arena * arena, Str str, Str file_name);
+ExprResult parse_expression(Arena * arena,Token * tokens, size_t count);
+
 
